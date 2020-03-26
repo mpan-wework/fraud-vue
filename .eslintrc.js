6680dd8c027c3d298bcc0043baed1bf4ce9e70fd
Mike Pan <mike.pan@wework.com>
2020-03-26 20:47:24 +0800

    chore: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,39 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-  }
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+        quoteProps: 'as-needed',
+        jsxSingleQuote: false,
+        trailingComma: 'es5',
+        bracketSpacing: true,
+        arrowParens: 'always',
+        requirePragma: false,
+        insertPragma: false,
+        vueIndentScriptAndStyle: false,
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
